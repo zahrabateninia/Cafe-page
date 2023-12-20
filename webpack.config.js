3571e8316999134fc5ec0cdf2a8e15f6bf743a49
zahrabateninia <zahrabatenin@gmail.com>
2023-12-20 23:38:47 +0330

    Change webpack.config.js file to be much shorter

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,10 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js', // Entry point of your application
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'main.js', // Output file name
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // Using Babel loader for JavaScript
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/, // For handling CSS files
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i, // For handling image files (PNG, JPG, JPEG, GIF)
-        type: 'asset/resource',
-      },
-      // Add more rules for handling other file types as needed
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: 'styles.css' }), // Extract CSS into a separate file
-    // Add more plugins if required
-  ],
-  // Define other webpack configurations, optimization settings, etc.
 };
