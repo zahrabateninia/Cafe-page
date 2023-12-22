e18ea699e2ed9d8c46bfbcf5acf8b4aca50f6f35
zahrabateninia <zahrabatenin@gmail.com>
2023-12-22 17:43:00 +0330

    Implement the swiper slider
    use this website for swiper: https://swiperjs.com/demos#autoplay

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".home-slider", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        loop: true,
+    });
+});
